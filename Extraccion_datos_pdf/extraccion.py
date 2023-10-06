73e672435af8188693d718b1af7c10c344912374
nitramic <martin.cima@gmail.com>
2023-10-06 16:21:20 +0200

    open_pdfs

---
+++ Extraccion_datos_pdf/extraccion.py
@@ -0,0 +1,10 @@
+## #pip install fitz  Lo saco porque no funciona, ver nota en import
+## pip install PyMuPDF
+## pip install frontend
+## pip install pandas
+## pip install re
+
+import fitz ## Ojo, es reemplazado por PyMuPDF ya que fitz original no funciona bien, pero se llama en el import como fitz
+import re
+import pandas
+
